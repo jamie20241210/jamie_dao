b0d64fb6855303070da92707bd279c502a16c9a5
Jamie <jamie20241210@gmail.com>
2025-05-11 17:03:27 +0700

    20250511

---
+++ git_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 检查是否提供了提交信息
+if [ -z "$1" ]; then
+  echo "请提供提交信息。用法: ./git_commit_push.sh '提交信息'"
+  exit 1
+fi
+
+# 添加所有更改的文件
+git add .
+
+# 提交更改
+git commit -m "$1"
+
+# 推送到远程仓库
+git push
+
+echo "提交和推送完成！"
